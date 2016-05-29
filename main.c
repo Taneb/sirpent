de8076a5215b2352c29b43100d577405f53f266f
Michael Mokrysz <hi@46b.it>
2016-05-29 15:29:34 +0100

    Start of refactoring into segment linked lists.

--- main.c
+++ main.c
@@ -14,8 +14,7 @@ struct snake_segment {
   struct snake * snake_id;
   struct snake_segment * forward; /* nullptr if head */
   struct snake_segment * rearward; /* nullptr if tip of tail */
-  int x_coord;
-  int y_coord;
+  struct vector location;
 };
 
 static struct snake_segment * grid0 [GRID_SIZE][GRID_SIZE];
@@ -31,108 +30,89 @@ typedef enum {
   NORTHWEST
 } direction;
 
+struct vector {
+  int x, y;
+};
+
 direction ask_direction(struct snake snake) {
   /* TODO actually ask */
   return SOUTH;
 }
 
-int is_out_of_bounds(int x_coord, int y_coord) {
-  return x_coord < 0 || x_coord >= GRID_SIZE || y_coord < 0 || y_coord >= GRID_SIZE;
+int is_out_of_bounds(struct vector cell) {
+  return cell.x < 0 || cell.x >= GRID_SIZE || cell.y < 0 || cell.y >= GRID_SIZE;
+}
+
+struct vector neighbour(struct vector cell, direction to_neighbour) {
+  struct vector neighbour = cell;
+  switch (to_neighbour) {
+  case NORTH:
+    neighbour.y--;
+    break;
+  case NORTHEAST:
+    neighbour.x++;
+    neighbour.y--;
+    break;
+  case SOUTHEAST:
+    neighbour.x++;
+    break;
+  case SOUTH:
+    neighbour.y++;
+    break;
+  case SOUTHWEST:
+    neighbour.x--;
+    neighbour.y++;
+    break;
+  case NORTHWEST:
+    neighbour.x--;
+    break;
+  }
+  return neighbour;
 }
 
 void update_world() {
-  int x; int y;
-  struct linked_list * snakes_to_remove = new();
-  for (x = 0; x < GRID_SIZE; x++) {
-    for (y = 0; y < GRID_SIZE; y++) {
-      if (grid0[x][y]) {
-	if (grid0[x][y]->forward) {
-	  /* we are in the tail of a snake */
-	  struct snake_segment forward = *grid0[x][y]->forward;
-	  grid1[forward.x_coord][forward.y_coord] = grid0[x][y];
-	}
-	else {
-	  /* we are in the head of a snake */
-	  direction which_way = ask_direction(*grid0[x][y]->snake_id);
-	  int new_x = x, new_y = y;
-	  switch (which_way) {
-	  case NORTH:
-	    new_y -= 1;
-	    break;
-	  case NORTHEAST:
-	    new_x += 1;
-	    new_y -= 1;
-	    break;
-	  case SOUTHEAST:
-	    new_x += 1;
-	    break;
-	  case SOUTH:
-	    new_y += 1;
-	    break;
-	  case SOUTHWEST:
-	    new_x -= 1;
-	    new_y += 1;
-	    break;
-	  case NORTHWEST:
-	    new_x -= 1;
-	    break;
-	  }
-	  
-	  /* CHECK FOR COLLISIONS */
-	  if (is_out_of_bounds(new_x, new_y)) {
-	    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
-	    continue;
-	  }
-	  if (grid1[new_x][new_y]) {
-	    /* collision */
-	    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
-	    continue;
-	  }
-	  if (grid2[new_x][new_y]) {
-	    /* head on collision */
-	    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
-	    snakes_to_remove = prepend(grid2[new_x][new_y]->snake_id, snakes_to_remove);
-	    continue;
-	  }
-	  
-	  grid2[new_x][new_y] = grid0[x][y];
-	}
-      }
+  int s;
+  for (s = 0; s < snakes_len; s++) {
+    struct snake *snake = snakes[s];
+
+    direction snake_direction = ask_direction(snake);
+    struct vector new_head_position = neighbour(snake->head.position, snake_direction);
+    if (is_out_of_bounds(new_head_position)) {
+      /* @TODO: REMOVE SNAKE */
+      continue;
     }
-  }
-  for (x = 0; x < GRID_SIZE; x++) {
-    for (y = 0; y < GRID_SIZE; y++) {
-      if (grid1[x][y]) {
-	if (search(grid1[x][y]->snake_id, snakes_to_remove)) {
-	  grid0[x][y] = 0;
-	  free(grid1[x][y]);
-	}
-	else {
-	  grid0[x][y] = grid1[x][y];
-	}
-	grid1[x][y] = 0;
-      }
-      else if (grid2[x][y]) {
-	if (search(grid2[x][y]->snake_id, snakes_to_remove)) {
-	  grid0[x][y] = 0;
-	  free(grid2[x][y]);
-	}
-	else {
-	  grid0[x][y] = grid2[x][y];
-	}
-	grid2[x][y] = 0;
-      }
-      else {
-	grid0[x][y] = 0;
-      }
-      if (grid0[x][y]) {
-	grid0[x][y]->x_coord = x;
-	grid0[x][y]->y_coord = y;
-      }
+
+    /* Move each segment forward by 1. */
+    struct snake_segment *current_segment = snake->head;
+    while (current_segment->next != NULL) {
+      current_segment->next.position = current_segment->position;
+      current_segment = current_segment->next;
     }
+    /* Update the head segment position. */
+    snake->head.position = new_head_position;
   }
 
+  /* @TODO: Collision detection. What are the rules for collisions? */
+
+  /*
+  CHECK FOR COLLISIONS
+  if (is_out_of_bounds(new_head_cell)) {
+    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
+    continue;
+  }
+  if (grid1[new_head_cell.x][new_head_cell.y]) {
+    collision
+    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
+    continue;
+  }
+  if (grid2[new_head_cell.x][new_head_cell.y]) {
+    head on collision
+    snakes_to_remove = prepend(grid0[x][y]->snake_id, snakes_to_remove);
+    snakes_to_remove = prepend(grid2[new_head_cell.x][new_head_cell.y]->snake_id, snakes_to_remove);
+    continue;
+  }
   destroy(snakes_to_remove);
+  */
 }
 
 int main() {
